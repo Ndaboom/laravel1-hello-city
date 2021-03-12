d422bb5840469581910ec25bba829e6f48eaf8e7
Sammy Ndabo <ndabosam084@gmail.com>
2021-03-12 14:07:06 -0600

    Function asset helper usage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,7 @@
 @section('title', ''.config('app.name'))
 
 @section('content')
-        <img src="/images/city.jpg" alt="city image" style="width:100px;">
+        <img src="{{ asset('/images/city.jpg') }}" alt="city image" style="width:100px;">
         <h1>Hello from Goma!</h1>
 
         <p>It's currently {{ date('h:i A') }}.</p>
